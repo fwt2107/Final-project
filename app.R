950081952877f43d29f4cc241c3c44a4f02eb036
Felix Tran <fwt2107@cumc.columbia.edu>
2018-12-04 18:03:41 -0500

    Changed app.R

--- app.R
+++ app.R
@@ -1,6 +1,5 @@
-rsconnect::setAccountInfo(name = 'fwt2107', 
-                          token = '334507B41A1EB727C94880B1393BE975', 
-                          secret = 'aTIDGArAMy485ZDYXo/bvKCeCmtbqS1mAY46agwn')
-
 library(rsconnect)
-rsconnect::deployApp('./shiny_dashboard.Rmd')
+setAccountInfo(name = 'fwt2107', 
+               token = '334507B41A1EB727C94880B1393BE975', 
+               secret = 'aTIDGArAMy485ZDYXo/bvKCeCmtbqS1mAY46agwn')
+deployApp('./shiny_dashboard.Rmd')
